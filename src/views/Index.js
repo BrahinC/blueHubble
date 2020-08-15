a910fcad1b003ee5409102eb2d9cf2b0f06deeaf
GabrielaMachado <gabimachadop@hotmail.com>
2020-08-14 19:51:18 -0500

    first commit

---
+++ src/views/Index.js
@@ -0,0 +1,45 @@
+import React from "react";
+
+// reactstrap components
+// import {
+// } from "reactstrap";
+
+// core components
+import IndexNavbar from "components/Navbars/IndexNavbar.js";
+import IndexHeader from "components/Headers/IndexHeader.js";
+
+// sections for this page
+import Cards from "./index-sections/Cards.js";
+import Team from "./index-sections/Team.js";
+import Forms from "./index-sections/Forms.js";
+import TransparentFooter from "components/Footers/TransparentFooter.js";
+
+function Index() {
+  React.useEffect(() => {
+    document.body.classList.add("index-page");
+    document.body.classList.add("sidebar-collapse");
+    document.documentElement.classList.remove("nav-open");
+    window.scrollTo(0, 0);
+    document.body.scrollTop = 0;
+    return function cleanup() {
+      document.body.classList.remove("index-page");
+      document.body.classList.remove("sidebar-collapse");
+    };
+  });
+  return (
+    <>
+      <IndexNavbar />
+      <div className="wrapper">
+        <IndexHeader />
+        <div className="main">
+          <Cards />
+          <Team />
+          <Forms />
+        </div>
+        <TransparentFooter />
+      </div>
+    </>
+  );
+}
+
+export default Index;
